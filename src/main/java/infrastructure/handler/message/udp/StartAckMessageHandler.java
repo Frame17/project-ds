743455a3d2eee7a4c66bc46385303c3cf165d2e4
Serhii Fedusov <fedusov.serhiy@gmail.com>
2022-02-13 21:58:59 +0100

    finilize

--- src/main/java/infrastructure/handler/message/udp/StartAckMessageHandler.java
+++ src/main/java/infrastructure/handler/message/udp/StartAckMessageHandler.java
@@ -3,8 +3,6 @@ package infrastructure.handler.message.udp;
 import infrastructure.Command;
 import infrastructure.Node;
 import infrastructure.client.RemoteClient;
-import infrastructure.converter.ElectionPayloadConverter;
-import infrastructure.converter.NeighbourInfoPayloadConverter;
 import infrastructure.converter.PayloadConverter;
 import infrastructure.system.Leader;
 import infrastructure.system.LeaderContext;
@@ -19,7 +17,6 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.InetAddress;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -29,11 +26,15 @@ public class StartAckMessageHandler implements UdpMessageHandler {
     private final ScheduledExecutorService threadPool = Executors.newScheduledThreadPool(1);
     private final RemoteClient<DatagramPacket> client;
     private final PayloadConverter<StartAckMessage> converter;
+    private final PayloadConverter<ElectionMessage> electionMessageConverter;
+    private final PayloadConverter<NeighbourInfoMessage> neighbourInfoMessageConverter;
     private final PayloadConverter<HealthMessage> healthPayloadConverter;
 
-    public StartAckMessageHandler(RemoteClient<DatagramPacket> client, PayloadConverter<StartAckMessage> converter, PayloadConverter<HealthMessage> healthPayloadConverter) {
+    public StartAckMessageHandler(RemoteClient<DatagramPacket> client, PayloadConverter<StartAckMessage> converter, PayloadConverter<ElectionMessage> electionMessageConverter, PayloadConverter<NeighbourInfoMessage> neighbourInfoMessageConverter, PayloadConverter<HealthMessage> healthPayloadConverter) {
         this.client = client;
         this.converter = converter;
+        this.electionMessageConverter = electionMessageConverter;
+        this.neighbourInfoMessageConverter = neighbourInfoMessageConverter;
         this.healthPayloadConverter = healthPayloadConverter;
     }
 
@@ -68,7 +69,7 @@ public class StartAckMessageHandler implements UdpMessageHandler {
                     }
 
                     if (context.getNeighbour() != null && context.getNeighbour().equals(new RemoteNode(context.getLeader().ip(), context.getLeader().port()))) {
-                        client.unicast(new NeighbourInfoPayloadConverter().encode(Command.NEIGHBOUR_INFO, new NeighbourInfoMessage(new RemoteNode(Node.getLocalIp(), context.listenPort))),
+                        client.unicast(neighbourInfoMessageConverter.encode(Command.NEIGHBOUR_INFO, new NeighbourInfoMessage(new RemoteNode(Node.getLocalIp(), context.listenPort))),
                                 context.getLeader().ip(), context.getLeader().port());
                     }
                 }
@@ -92,7 +93,7 @@ public class StartAckMessageHandler implements UdpMessageHandler {
                 context.setElectionParticipant(true);
                 ElectionMessage message = new ElectionMessage(new RemoteNode(Node.getLocalIp(), context.listenPort), false);
 
-                client.unicast(new ElectionPayloadConverter().encode(Command.ELECTION, message),
+                client.unicast(electionMessageConverter.encode(Command.ELECTION, message),
                         context.getNeighbour().ip(), context.getNeighbour().port());
             }
         } catch (IOException e) {
